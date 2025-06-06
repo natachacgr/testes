6fdcb7f6f6f0a670001f2fbb061bab7367238768
Gabriel Almeida Dias <bielalmeidadias@hotmail.com>
2025-06-06 16:37:28 -0300

    Testes do login admin e verificar pedidos do admin

---
+++ TestesUI/cypress/e2e/loginAdm.cy.js
@@ -0,0 +1,40 @@
+describe("Testes de Login - Elo Drinks", () => {
+  beforeEach(() => {
+    cy.visit("https://elodrinks.confianopai.com/");
+  });
+
+  it("Deve acessar a página de login", () => {
+    // Procurar por botões/links de login na página principal
+    cy.get("body").then(($body) => {
+      if (
+        $body.find('[data-cy="login"], a[href*="login"], button').length > 0
+      ) {
+        cy.get('[id^="login"], a[href*="login"], button')
+          .contains(/login|entrar/i)
+          .first()
+          .click();
+      } else {
+        // Se não encontrar, navegar diretamente
+        cy.visit("https://elodrinks.confianopai.com/login");
+      }
+    });
+
+    cy.url().should("include", "login");
+    cy.get('input[id^="email"]').should("be.visible");
+    cy.get('input[type="password"]').should("be.visible");
+  });
+
+  it("Deve realizar login com credenciais válidas e ir para o dashboard", () => {
+    cy.get("a, button").contains(/login/i).first().click();
+
+    cy.get('input[id^="email"]:visible')
+      .should("be.visible")
+      .type("gabriel@gabriel");
+
+    cy.get('input[id^="senha"]:visible').should("be.visible").type("1234");
+
+    cy.contains('button:visible', /Entrar/i).click({ force: true });
+
+    cy.location("pathname", { timeout: 15000 }).should("eq", "/dashboard");
+  });
+});
---
+++ TestesUI/cypress/e2e/pedidosAdmin.cy.js
@@ -0,0 +1,42 @@
+describe("Admin - Lista de Pedidos (Elo Drinks)", () => {
+  const email = "gabriel@gabriel";
+  const senha = "1234";
+
+  beforeEach(() => {
+    cy.visit("https://elodrinks.confianopai.com/");
+
+    // Entrar na tela de login
+    cy.get("body").then(($body) => {
+      if (
+        $body.find('[data-cy="login"], a[href*="login"], button')
+          .length > 0
+      ) {
+        cy.contains("a, button", /login|entrar/i).first().click();
+      } else {
+        cy.visit("https://elodrinks.confianopai.com/login");
+      }
+    });
+
+    // Fazer login
+    cy.get('input[id^="email"]:visible').type(email);
+    cy.get('input[id^="senha"]:visible').type(senha);
+
+    cy.contains('button:visible', /Entrar/i).click({ force: true });
+
+    cy.location("pathname", { timeout: 1000 }).should("eq", "/dashboard");
+  });
+
+  it("Deve abrir o sidebar, acessar /pedidos e encontrar Casamento Natacha e Otavio", () => {
+    cy.get('button[class*="lg:hidden"]')
+      .filter(":visible")
+      .first()
+      .click();
+
+    cy.get(':nth-child(3) > .flex').first().click();
+
+    cy.location("pathname").should("eq", "/pedidos");
+
+    cy.contains("tbody tr", "Casamento Natacha e Otavio", { timeout: 1000 })
+      .should("be.visible");
+  });
+});
